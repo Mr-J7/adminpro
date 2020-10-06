51f75b8d986a21d1a63c337554cf82d803779eb5
Mr-J <jvrg77@gmail.com>
2020-10-05 23:17:11 -0400

    Modificaciones en modulos e implementacion de rutas 2

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+import { PagesRoutingModule } from './pages-routing.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+
+
+@NgModule({
+  declarations: [
+     DashboardComponent,
+     ProgressComponent,
+     Graficas1Component,
+     PagesComponent
+    ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    PagesRoutingModule
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent
+  ]
+})
+export class PagesModule { }
